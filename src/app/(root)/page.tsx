4f2a72d70200a42f73c23c12188609eac156aa26
Amardeep840690 <amar840690@gmail.com>
2026-07-25 09:18:42 +0530

    feat: add KEC website footer component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,7 +3,7 @@
 export default async function Home() {
 
   return (
-    <main>
+    <main className="flex flex-col items-center justify-between p-24">
       Home Page
     </main>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+        <Footer/>
       </body>
     </html>
   );
